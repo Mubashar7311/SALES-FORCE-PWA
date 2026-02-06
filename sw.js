7131afee8c65a0f281ac4c65c41a1320bf417b19
Mubashar7311 <fbs7311@gmail.com>
2026-02-06 13:04:31 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open("sales-cache").then(c=>{
-return c.addAll(["./","index.html"])
-})
-)
-})
-
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-)
-})
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open('cache').then(c=>c.addAll(['index.html'])));
+});
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
